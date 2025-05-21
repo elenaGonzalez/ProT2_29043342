837c0d5d9c2fb748a9c615145f62d4a541aa1295
elenaGonzalez <elebeatrizgonzakez@gmail.com>
2025-05-20 21:37:41 -0300

    Agrega iconos, emojies y estilos

--- app/Views/front/principal_view.php
+++ app/Views/front/principal_view.php
@@ -1,8 +1,9 @@
   <script src="assets/js/bootstrap.bundle.min.js"></script>
   <section>
-    <div class="text-center text-bg-light p-3">
-      <p class="text-primary p-3 fw-bolds fst-italic">Corrientes: Naturalmente Mágica 🐊</p>
-      <p>Turismo Corrientes nace como una empresa que ofrece verdaderas aventuras ecológicas 
+    <!--<p class="text-primary p-3 fw-bolds fst-italic">Corrientes: Naturalmente Mágica 🐊</p> -->
+    <div class="text-center text-success-emphasis p-2 text-white bg-opacity-90 p-3">
+      
+      <p><strong>Turismo Corrientes </strong> nace como una empresa que ofrece verdaderas aventuras ecológicas
         que no puedes dejar de conocer.</p>
       <p> Te ofrecemos las mejores excusiones y paseos, para que vivas a pleno tu estadía en Corrientes!!</p>
       <p> Si pensas en un lugar donde pasar momentos inolvidables,
@@ -10,26 +11,44 @@
     </div>
   </section>
   <!-- carrousell -->
-  <div id="carouselExample" class="carousel slide">
+  <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+     <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+  </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img src="assets/img/puente.png" class="d-block w-100" alt="puente">
+        <div class="carousel-caption text-sm bg-black opacity-50">
+          <h5>Excursiones a Playas doradas todo el año 🏖️</h5>
+        </div>
       </div>
       <div class="carousel-item">
         <img src="assets/img/capibara.png" class="d-block w-100" alt="capibara">
+        <div class="carousel-caption text-sm bg-black opacity-50">
+          <h5>Paquetes a los Esteros del Iberá 🐊</h5>
+        </div>
       </div>
       <div class="carousel-item">
         <img src="assets/img/empedrado.png" class="d-block w-100" alt="img3">
+        <div class="carousel-caption text-sm bg-black opacity-50">
+          <h5>Conocé la Perla del Paraná 🏕️</h5>
+        </div>
       </div>
       <div class="carousel-item">
         <img src="assets/img/itati.png" class="d-block w-100" alt="itati">
+        <div class="carousel-caption text-sm bg-black opacity-50">
+          <h5>Visitá la Basílica de Itatí 🙏</h5>
+        </div>
       </div>
     </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Anterior</span>
     </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="visually-hidden">Siguiente</span>
     </button>
